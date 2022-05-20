43e1c43baba53d46e97c1dc902c73e786efbc30a
AravindSudikonda <aravindsudikonda10@example.com>
2022-05-21 04:42:38 +0530

    added snakeladder position 0

--- src/comblsnakeladder/SnakeLadder.java
+++ src/comblsnakeladder/SnakeLadder.java
@@ -3,5 +3,6 @@ package comblsnakeladder;
 public class SnakeLadder {
     public static void main(String[] args){
         System.out.println("Welcome to Snake and Ladder");
+        int postion = 0;
     }
 }
